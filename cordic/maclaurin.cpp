ecab00a4df761b9047fc0fa72f17615af0c03f36
yudai <yudai.yy0804@gmail.com>
2024-03-16 13:54:43 +0900

    feat: ベルヌーイ数を使ったtanを実装したけど、精度がでなかった

--- cordic/maclaurin.cpp
+++ cordic/maclaurin.cpp
@@ -76,9 +76,14 @@ double sin(double x, int n) {
   // nを1,3,5,...から0,1,2,...の並びにする
   n = n / 2;
   double y = 0;
+  double sign = 1.0;
+  double px = x;
+  double f = 1.0;
   for (int i = 0; i < n; i++) {
-    double sign = (i % 2 == 0) ? 1 : -1;
-    y += sign / factorial(2 * i + 1) * power(x, 2 * i + 1);
+    y += sign / f * px;
+    sign = -sign;
+    f *= (double)(2 * (i + 1) + 1) * (double)(2 * (i + 1));
+    px *= x * x;
   }
   return y;
 }
@@ -97,13 +102,48 @@ double cos(double x, int n) {
   // nを0,2,4,...から0,1,2,...の並びにする
   n = n / 2;
   double y = 0;
+  double sign = 1.0;
+  double px = 1;
+  double f = 1.0;
   for (int i = 0; i < n; i++) {
-    double sign = (i % 2 == 0) ? 1 : -1;
-    y += sign / factorial(2 * i) * power(x, 2 * i);
+    y += sign / f * px;
+    sign = -sign;
+    f *= (double)(2 * (i + 1)) * (double)(2 * (i + 1) - 1);
+    px *= x * x;
   }
   return y;
 }
-double tan(double a);
+/**
+ * @brief
+ * @note |x|=pi/2付近の精度が出なかったため没
+ * @param x
+ * @param n 1,3,5,...
+ * @return double
+ */
+double tan_bernoulli(double x, int n) {
+  if (n % 2 == 0) {
+    printf("n = %d is error\r\n", n);
+    return 0;
+  }
+  n = n / 2 + 1;
+  double y = 0;
+  double sign = -1.0;
+  // pn = 4^n
+  double pn = 4;
+  double px = x;
+  double f = 2;
+  for (int i = 1; i <= n; i++) {
+    y += bernoulli(2 * i) * sign * pn * (1.0 - pn) / f * px;
+    sign = -sign;
+    pn *= 4.0;
+    f *= (double)(2 * (i + 1)) * (double)(2 * (i + 1) - 1);
+    px *= x * x;
+  }
+  return y;
+}
+
+double tan(double x, int n) { return sin(x, n) / cos(x, n - 1); }
+
 double asin(double a);
 double acos(double a);
 double atan(double a);
@@ -121,11 +161,13 @@ double sqrt(double a);
 
 int main(void) {
   printf("program start\r\n");
-  // for (double i = -M_PI / 2; i < M_PI / 2; i += 0.01) {
-  //   printf("%f %f %f\r\n", i, std::cos(i), maclaurin::cos(i, 6));
-  // }
-  for (int i = 0; i < 7; i++) {
-    printf("%d %f\r\n", i, maclaurin::bernoulli(i));
+  for (double i = -M_PI / 2; i < M_PI / 2; i += 0.01) {
+    printf("%f %f %f, %f\r\n", i, std::tan(i), maclaurin::tan(i, 19),
+           maclaurin::sin(i, 19) / maclaurin::cos(i, 18));
+    // printf("%f %f %f\r\n", i, std::cos(i), maclaurin::cos(i, 18));
   }
+  // for (int i = 0; i < 7; i++) {
+  //   printf("%d %f\r\n", i, maclaurin::bernoulli(i));
+  // }
   return 0;
 }
